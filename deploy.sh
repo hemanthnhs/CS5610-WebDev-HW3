fa254c9f8f76d9f6c2724884ef7425c5dba286a1
Hemanth Sai <hemanthsai10@gmail.com>
2019-09-19 16:04:58 -0400

    Update deploy file to fix start issue

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,10 @@ mix phx.digest
 echo "Generating release..."
 mix release
 
-#echo "Stopping old copy of app, if any..."
-#_build/prod/rel/practice/bin/practice stop || true
+echo "Stopping old copy of app, if any..."
+_build/prod/rel/practice/bin/practice stop || true
 
 echo "Starting app..."
 
-_build/prod/rel/practice/bin/practice foreground
+_build/prod/rel/practice/bin/practice start
 
