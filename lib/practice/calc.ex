cd4353240a422b36e3367d6dbe3ffee13d92c1a4
Hemanth Sai <hemanthsai10@gmail.com>
2019-09-18 22:27:15 -0400

    Added token and single calc a+b functionality

--- lib/practice/calc.ex
+++ lib/practice/calc.ex
@@ -4,14 +4,35 @@ defmodule Practice.Calc do
     num
   end
 
+  def tag_token(expr) do
+    # Referred to https://elixirschool.com/en/lessons/basics/control-structures/
+    IO.inspect expr
+    case expr do
+        n when n in ["+","-","*","/"] ->
+            {:op,expr}
+        _ ->
+            {:num,parse_float(expr)}
+    end
+  end
+
+  def tag_tokens(splts) do
+    # Referred to https://elixirschool.com/en/lessons/basics/enum/#map
+    Enum.map(splts,fn(x) -> tag_token(x) end)
+  end
+
+  def convert_to_postfix(splts) do
+    case splts do
+        [{:num,num1},{:op,"+"},{:num,num2} | tail] -> (num1+num2)
+    end
+  end
+
   def calc(expr) do
     # This should handle +,-,*,/ with order of operations,
     # but doesn't need to handle parens.
     expr
     |> String.split(~r/\s+/)
-    |> hd
-    |> parse_float
-    |> :math.sqrt()
+    |> tag_tokens
+    |> convert_to_postfix
 
     # Hint:
     # expr
